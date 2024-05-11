ca81e51c12390a56cf14bb819550bbd077678395
VilanSiriwardana <vilansiriwardana@gmail.com>
2024-05-11 13:28:01 +0530

    Create and Read

--- app/src/main/java/com/example/madlabexam4/MainActivity.kt
+++ app/src/main/java/com/example/madlabexam4/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.madlabexam4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.madlabexam4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+    private lateinit var db: NoteDatabaseHelper
+    private lateinit var notesAdapter: NotesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = NoteDatabaseHelper(this)
+        notesAdapter = NotesAdapter(db.getAllNotes(), this)
+
+        binding.notesRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.notesRecyclerView.adapter = notesAdapter
+
+        binding.addButton.setOnClickListener{
+            val intent = Intent(this,AddNoteActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        notesAdapter.refreshData(db.getAllNotes())
     }
 }
